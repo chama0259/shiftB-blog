209130610c112afc9faa7a2eb3700a9f4da5f2a6
chama0259 <chamaaadev0259@gmail.com>
2025-12-31 15:58:29 +0900

    その他UI改善

--- shiftB-blog/vite.config.js
+++ shiftB-blog/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
+//ファストリフレッシュ対応
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 })
+//↑「JSXはReactとして解釈してね」,「tailwindも解釈してね」
