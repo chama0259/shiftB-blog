c76a9bf4e77a477d8dbfd2c01c4b42af6bbc707c
chama0259 <chamaaadev0259@gmail.com>
2026-01-11 20:26:11 +0900

    詳細ページもAPI使用するよう修正。併せて一覧もPosts.jsxでAPI取得するよう修正。 loading中の表示を追加

--- shiftB-blog/src/App.jsx
+++ shiftB-blog/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header.jsx";
@@ -6,26 +5,13 @@ import Posts from "./pages/Posts.jsx";
 import PostDetail from "./pages/PostDetail.jsx";
 
 function App() {
-  const [posts, setPosts] = useState([]);
-  useEffect(() => {
-    const fetcher = async () => {
-      const res = await fetch(
-        "https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts"
-      );
-      const data = await res.json();
-      setPosts(data.posts);
-    };
-
-    fetcher();
-  }, []);
-
   return (
     <>
       <Header />
       <main className="main">
         <Routes>
-          <Route path="/" element={<Posts posts={posts} />} />
-          <Route path="/posts/:id" element={<PostDetail posts={posts} />} />
+          <Route path="/" element={<Posts />} />
+          <Route path="/posts/:id" element={<PostDetail />} />
         </Routes>
       </main>
     </>
