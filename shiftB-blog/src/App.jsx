bfe6458e8581dbd75a93dc61156ef6e2e28ef237
chama0259 <chamaaadev0259@gmail.com>
2026-01-11 18:48:51 +0900

    課題3の実装

--- shiftB-blog/src/App.jsx
+++ shiftB-blog/src/App.jsx
@@ -1,12 +1,24 @@
-import { Fragment } from "react";
+import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header.jsx";
-import { posts } from "./data/posts.js";
 import Posts from "./pages/Posts.jsx";
 import PostDetail from "./pages/PostDetail.jsx";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const fetcher = async () => {
+      const res = await fetch(
+        "https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts"
+      );
+      const data = await res.json();
+      setPosts(data.posts);
+    };
+
+    fetcher();
+  }, []);
+
   return (
     <>
       <Header />
